c0e520a614b3ebdc7cb94088d9485bc5e1602f0e
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-31 15:46:06 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const cats = ["Milo", "Otis", "Garfield"];
     cats.push("Ralph");
     console.log(cats);
+    function destructivelyAppendCat(name) 
+    function destructivelyPrependCat(name)
+    function destructivelyRemoveLastCat()
+    function destructivelyRemoveFirstCat()
+    function appendCat(name)
+    function prependCat(name)
+    function removeLastCat() 
+    function removeFirstCat() 
