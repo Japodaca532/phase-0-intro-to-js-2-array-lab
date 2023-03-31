3df45dd6e287ad4b5f2592bb7558d930ce8dcd5d
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-31 15:40:49 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Write your solution here!
-function destructivelyAppendCat(name) {
-    const cats = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo", "Otis", "Garfield"];
     cats.push("Ralph");
     console.log(cats);
-}
